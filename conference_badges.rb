6f2c73252c253167dcb0aa67b2dff62a649829e7
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 00:16:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,28 +1,28 @@
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
-
-def batch_badge_creator(speakers)
-  speakers.collect do |speaker|
-    badge_maker(speaker)
-  end
-end
-
-
-def assign_rooms(speakers)
-  speakers.map.each_with_index do |speaker, index|
-    "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
-  end
-end
-
-def printer(speakers)
-  batch_badge_creator(speakers).each do |badge|
-    puts badge
-  end
-  assign_rooms(speakers).each do |room|
-    puts room
-  end
-end
+# def badge_maker(name)
+#   return "Hello, my name is #{name}."
+# end
+#
+# def batch_badge_creator(speakers)
+#   speakers.collect do |speaker|
+#     badge_maker(speaker)
+#   end
+# end
+#
+#
+# def assign_rooms(speakers)
+#   speakers.map.each_with_index do |speaker, index|
+#     "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+#   end
+# end
+#
+# def printer(speakers)
+#   batch_badge_creator(speakers).each do |badge|
+#     puts badge
+#   end
+#   assign_rooms(speakers).each do |room|
+#     puts room
+#   end
+# end
 
 
 # def badge_maker(name)
@@ -30,7 +30,7 @@ end
 # end
 
 # def batch_badge_creator(attendees)
-#   attendees.collect do |name| 
+#   attendees.collect do |name|
 #   badge_maker(name)
 #   end
 # end
@@ -49,3 +49,22 @@ end
 #   puts room
 # end
 # end
+
+
+def ordered_count("h")
+  results_array = []
+  string_array = str.scan /\w/
+  #=> ["a", "b", "r", "a", "c", "a", "d", "a", "b", "r", "a"]
+  string_array.each do |letter|
+    if !results_array.include?(letter)
+      results_array.push([letter, 1])
+    binding.pry
+    end
+  end
+end
+
+str= "abracadabra"
+
+ordered_count("abracadabra")
+
+=> ["a", "b", "r", "a", "c", "a", "d", "a", "b", "r", "a"]
